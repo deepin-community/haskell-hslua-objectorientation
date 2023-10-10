77d7bbcf2926376a27e60c8c84ecdfa17a98b854
bluesky <chenchongbiao@deepin.org>
2023-10-10 13:22:28 +0800

    chore: init repo
    Object orientation tools for HsLua
    
    Issue: https://github.com/linuxdeepin/developer-center/issues/5731
    Log: init repo

---
+++ cbits/hslobj.c
@@ -0,0 +1,332 @@
+#include <HsFFI.h>
+#include <lua.h>
+#include <lauxlib.h>
+#include <string.h>
+
+/* ***************************************************************
+ * Helpers for fast element access
+ * ***************************************************************/
+
+/*
+** Pushes the caching table of the userdata at index `idx` to the stack.
+**
+** Creates and sets a new table if none has been attached to the
+** userdata yet.
+*/
+void hsluaO_get_caching_table(lua_State *L, int idx)
+{
+  int absidx = lua_absindex(L, idx);
+  if (lua_getuservalue(L, idx) == LUA_TTABLE) {
+    return;
+  }
+
+  /* No caching table set yet; create table and add to object. */
+  lua_pop(L, 1);                        /* remove nil */
+  lua_createtable(L, 0, 0);
+  lua_pushvalue(L, -1);
+  lua_setuservalue(L, idx);
+}
+
+/*
+** Retrieve a value from the wrapped userdata project. The userdata must
+** be in position 1, and the key in position 2. Returns 1 if a value was
+** found and is at the top of the stack, 0 otherwise. Does not clean-up
+** on success.
+*/
+int hsluaO_get_from_cache(lua_State *L)
+{
+  /* Use value in caching table if present */
+  hsluaO_get_caching_table(L, 1);       /* table */
+  lua_pushvalue(L, 2);                  /* key */
+  if (lua_rawget(L, 3) == LUA_TNIL) {
+    lua_pop(L, 2);                      /* remove nil, caching table */
+    return 0;
+  }
+  /* found the key in the cache */
+  return 1;
+}
+
+
+/*
+** Retrieve a value from the wrapped userdata project.
+** The userdata must be in position 1, and the key in position 2.
+ */
+int hsluaO_get_via_getter(lua_State *L)
+{
+  /* Bail if there are no getterns, or no getter for the given key. */
+  if (luaL_getmetafield(L, 1, "getters") != LUA_TTABLE) {
+    return 0;
+  }
+  lua_pushvalue(L, 2);                  /* key */
+  if (lua_rawget(L, -2) == LUA_TNIL) {
+    lua_pop(L, 1);
+    return 0;
+  }
+
+  /* Call getter. Slow, as it calls into Haskell. */
+  lua_pushvalue(L, 1);
+  lua_call(L, 1, 1);
+
+  /* key found in wrapped userdata, add to caching table */
+  hsluaO_get_caching_table(L, 1);       /* object's caching table */
+  lua_pushvalue(L, 2);                  /* key */
+  lua_pushvalue(L, -3);                 /* value */
+  lua_rawset(L, -3);
+  lua_pop(L, 1);                        /* pop caching table */
+  /* return value */
+  return 1;
+}
+
+/*
+** Retrieve a value by using the key as the alias for a different
+** property. The userdata must be in position 1, and the key in position
+** 2.
+*/
+int hsluaO_get_via_alias(lua_State *L)
+{
+  if (luaL_getmetafield(L, 1, "aliases") != LUA_TTABLE) {
+    return 0;             /* no aliases available */
+  }
+  lua_pushvalue(L, 2);
+  if (lua_rawget(L, -2) != LUA_TTABLE) {
+    lua_pop(L, 2);       /* key is not an alias */
+    return 0;            /* try a different method */
+  }
+
+  /* key is an alias */
+  lua_pushvalue(L, 1);    /* start with the original object */
+  /* Iterate over properties; last object is on top of stack,
+   * list of properties is the second object. */
+  lua_Integer len = (lua_Integer) lua_rawlen(L, -2);
+  for (lua_Integer i = 1; i <= len; i++) {
+    lua_rawgeti(L, -2, i);
+    int objtype = lua_gettable(L, -2);  /* get property */
+    lua_remove(L, -2);    /* remove previous object */
+    if (!objtype) break;  /* abort if this property of the alias is absent */
+  }
+  return 1;
+}
+
+/*
+** Retrieve a method for this object. The userdata must be in position
+** 1, and the key in position 2.
+*/
+int hsluaO_get_method(lua_State *L)
+{
+  if (luaL_getmetafield(L, 1, "methods") != LUA_TTABLE) {
+    lua_pop(L, 1);
+    return 0;
+  }
+  lua_pushvalue(L, 2);
+  lua_rawget(L, -2);
+  return 1;
+}
+
+/*
+** Retrieve a numerical index from this object. The userdata must be in
+** position 1, and the key in position 2.
+*/
+int hsluaO_get_numerical(lua_State *L)
+{
+  hsluaO_get_caching_table(L, 1);
+  lua_Integer requested = lua_tointeger(L, 2);
+
+  /* The __lazylistindex is set to `nil` or an integer if part of the
+     list is still unevaluated. If it's `false`, then all list values are
+     already in the cache. */
+  if (lua_getfield(L, 1, "__lazylistindex") == LUA_TBOOLEAN) {
+    lua_pop(L, 1);                      /* remove nil */
+  } else {
+    lua_Integer last_index = lua_tointeger(L, -1);
+    lua_pop(L, 1);                      /* pop last-index value */
+
+    if (requested > last_index &&
+        /* index not in cache, force lazy evaluation of list items */
+        luaL_getmetafield(L, 1, "lazylisteval") == LUA_TFUNCTION) {
+      if (lua_getfield(L, 3, "__lazylist") != LUA_TUSERDATA) {
+        /* lazy list thunk is missing; that shouldn't happen!!  */
+        luaL_error(L, "Error while getting numerical index %d: "
+                   "lazy list thunk is missing", requested);
+      }
+      lua_pushinteger(L, last_index);
+      lua_pushinteger(L, requested);
+      lua_pushvalue(L, 3);              /* caching table */
+      lua_call(L, 4, 0);                /* populate cache with evaled values */
+    }
+  }
+  lua_rawgeti(L, 3, requested);
+  return 1;
+}
+
+/*
+** Retrieves a key from a Haskell-data holding userdata value.
+**
+** If the key is an integer, any associated list is evaluated and the
+** result is stored in the cache before it is returned.
+**
+** For non-integer keys, it tries the following, in order, and returns
+** the first non-nil result:
+**
+**   + Checks the userdata's uservalue table for the given key;
+**   + looks up a `getter` for the key and calls it with the userdata and
+**     key as arguments;
+**   + tries to lookup the key as an alias and retrieves the value of the
+**     alias;
+**   + looks up the key in the table in the `methods` metafield.
+*/
+int hslua_udindex(lua_State *L)
+{
+  lua_settop(L, 2);
+  /* do numeric lookup for integer keys */
+  return lua_isinteger(L, 2)
+    ? (hsluaO_get_via_alias(L) || hsluaO_get_numerical(L))
+    /* try various sources in order; return 0 if nothing is found. */
+    : (hsluaO_get_from_cache(L) ||
+       hsluaO_get_via_getter(L) ||
+       hsluaO_get_via_alias(L)  ||
+       hsluaO_get_method(L));
+}
+
+/*
+** Set value via a property alias. Assumes the stack to be in a state as
+** after __newindex is called. Returns 1 on success, and 0 otherwise.
+ */
+int hsluaO_set_via_alias(lua_State *L)
+{
+  if (luaL_getmetafield(L, 1, "aliases") != LUA_TTABLE) {
+    return 0;
+  }
+  lua_pushvalue(L, 2);
+  if (lua_rawget(L, -2) != LUA_TTABLE) { /* key is an alias */
+    lua_pop(L, 2);
+    return 0;
+  }
+  lua_pushvalue(L, 1);    /* start with the original object */
+  /* Iterate over properties; last object is on top of stack,
+   * list of properties is the second object. */
+  lua_Integer len = (lua_Integer) lua_rawlen(L, -2);
+  for (int i = 1; i < len; i++) {
+    lua_rawgeti(L, -2, i);
+    lua_gettable(L, -2);  /* get property */
+    lua_remove(L, -2);    /* remove previous object */
+  }
+  lua_rawgeti(L, -2, len); /* last element */
+  lua_pushvalue(L, 3);     /* new value */
+  lua_settable(L, -3);
+  return 1;
+}
+
+/*
+** Sets a numerical index on this object. The userdata must be in
+** position 1, the key in position 2, and the new value in position 3.
+** Returns 1 on success and 0 otherwise.
+*/
+int hsluaO_set_numerical(lua_State *L)
+{
+  hsluaO_get_caching_table(L, 1);
+  lua_Integer target = lua_tointeger(L, 2);
+
+  /* The `__lazylistindex` field is set to `false` if each list element
+     has already been evaluated and stored in the cache. Otherwise it
+     will be either `nil` or an integer. */
+  if (lua_getfield(L, 1, "__lazylistindex") == LUA_TBOOLEAN) {
+    lua_pop(L, 1);                      /* pop boolean from last-index */
+  } else {
+    /* list is not fully evaluated yet, we may have to evaluate it
+       further. */
+    lua_Integer last_index = lua_tointeger(L, -1);
+    lua_pop(L, 1);                      /* pop last-index value */
+
+    if (target > last_index) {
+      /* the index we want to assign has not been cached yet. Evaluation
+       * is forced to avoid any uncertainty about the meaning of
+       * `nil`-valued indices. */
+      lua_pushcfunction(L, &hsluaO_get_numerical);
+      lua_pushvalue(L, 1);
+      lua_pushvalue(L, 2);
+      lua_call(L, 2, 0);
+    }
+  }
+  lua_pushvalue(L, 3);                  /* new value */
+  lua_rawseti(L, -2, target);           /* set in caching table */
+  return 1;
+}
+
+/*
+** Set value via a property alias. Assumes the stack to be in a state as
+** after __newindex is called. Returns 1 on success, 0 if the object is
+** readonly, and throws an error if there is no setter for the given
+** key.
+*/
+int hsluaO_set_via_setter(lua_State *L)
+{
+  if (luaL_getmetafield(L, 1, "setters") != LUA_TTABLE)
+    return 0;
+
+  lua_pushvalue(L, 2);                  /* key */
+  if (lua_rawget(L, -2) != LUA_TFUNCTION) {
+    lua_pop(L, 1);
+    lua_pushliteral(L, "Cannot set unknown property.");
+    return lua_error(L);
+  }
+
+  lua_insert(L, 1);
+  lua_settop(L, 4);       /* 1: setter, 2: ud, 3: key, 4: value */
+  lua_call(L, 3, 0);
+  return 1;
+}
+
+/*
+** Sets a new value in the userdata caching table via a setter
+** functions.
+**
+** The actual assignment is performed by a setter function stored in the
+** `setter` metafield. Throws an error if no setter function can be
+** found.
+ */
+int hslua_udnewindex(lua_State *L)
+{
+  if (lua_type(L, 2) == LUA_TNUMBER) {
+    if (hsluaO_set_via_alias(L) || hsluaO_set_numerical(L)) {
+      return 0;
+    }
+    lua_pushliteral(L, "Cannot set a numerical value.");
+    return lua_error(L);
+  }
+  if (hsluaO_set_via_alias(L) || hsluaO_set_via_setter(L)) {
+    return 0;
+  }
+
+  lua_pushliteral(L, "Cannot modify read-only object.");
+  return lua_error(L);
+}
+
+/*
+** Sets a value in the userdata's caching table (uservalue). Takes the
+** same arguments as a `__newindex` function.
+*/
+int hslua_udsetter(lua_State *L)
+{
+  luaL_checkany(L, 3);
+  lua_settop(L, 3);
+  hsluaO_get_caching_table(L, 1);
+  lua_insert(L, 2);
+  lua_rawset(L, 2);
+  return 0;
+}
+
+/*
+** Throws an error noting that the given key is read-only.
+*/
+int hslua_udreadonly(lua_State *L)
+{
+  if (lua_type(L, 2) == LUA_TSTRING && lua_checkstack(L, 3)) {
+    lua_pushliteral(L, "'");
+    lua_pushvalue(L, 2);
+    lua_pushliteral(L, "' is a read-only property.");
+    lua_concat(L, 3);
+  } else {
+    lua_pushliteral(L, "Cannot set read-only value.");
+  }
+  return lua_error(L);
+}
---
+++ test/HsLua/ObjectOrientationTests.hs
@@ -0,0 +1,483 @@
+{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications  #-}
+{-|
+Module      : HsLua.ObjectOrientationTests
+Copyright   : © 2007–2012 Gracjan Polak;
+              © 2012–2016 Ömer Sinan Ağacan;
+              © 2017-2022 Albert Krewinkel
+License     : MIT
+Maintainer  : Albert Krewinkel <tarleb+hslua@zeitkraut.de>
+Stability   : beta
+Portability : non-portable (depends on GHC)
+
+Test that conversions from and to the Lua stack are isomorphisms.
+-}
+module HsLua.ObjectOrientationTests (tests) where
+
+import HsLua.Core
+import HsLua.ObjectOrientation
+import HsLua.Marshalling
+import Test.Tasty (TestTree, testGroup)
+import Test.Tasty.HsLua ((=:), shouldBeResultOf, shouldBeErrorMessageOf)
+import qualified Data.ByteString.Char8 as Char8
+
+-- | Tests for HsLua object orientation.
+tests :: TestTree
+tests = testGroup "Object Orientation"
+  [ testGroup "Sample product type"
+    [ "tostring" =:
+      "Foo 7 \"seven\"" `shouldBeResultOf` do
+        openlibs
+        pushUD typeFoo $ Foo 7 "seven"
+        setglobal "foo"
+        _ <- dostring "return tostring(foo)"
+        forcePeek $ peekText top
+
+    , "show" =:
+      "Foo 5 \"five\"" `shouldBeResultOf` do
+        openlibs
+        pushUD typeFoo $ Foo 5 "five"
+        setglobal "foo"
+        _ <- dostring "return foo:show()"
+        forcePeek $ peekText top
+
+    , "peek" =:
+      Foo 37 "ananas" `shouldBeResultOf` do
+        pushUD typeFoo $ Foo 37 "ananas"
+        forcePeek $ peekUD typeFoo top
+
+    , "unknown properties have value `nil`" =:
+      TypeNil `shouldBeResultOf` do
+        openlibs
+        pushUD typeFoo $ Foo (-1) "a"
+        setglobal "foo"
+        dostring "return foo.does_not_exist" >>= \case
+          OK -> ltype top
+          _ -> throwErrorAsException
+
+    , "get number" =:
+      (-1) `shouldBeResultOf` do
+        openlibs
+        pushUD typeFoo $ Foo (-1) "a"
+        setglobal "foo"
+        dostring "return foo.num" >>= \case
+          OK -> forcePeek $ peekIntegral @Int top
+          _ -> throwErrorAsException
+
+    , "get number twice" =:
+      8 `shouldBeResultOf` do
+        openlibs
+        pushUD typeFoo $ Foo 4 "d"
+        setglobal "foo"
+        dostring "return foo.num + foo.num" >>= \case
+          OK -> forcePeek $ peekIntegral @Int top
+          _ -> throwErrorAsException
+
+    , "modify number" =:
+      Foo (-1) "a" `shouldBeResultOf` do
+        openlibs
+        pushUD typeFoo $ Foo 1 "a"
+        setglobal "foo"
+        OK <- dostring "foo.num = -1"
+        TypeUserdata <- getglobal "foo"
+        forcePeek $ peekUD typeFoo top
+
+    , "get string" =:
+      "lint" `shouldBeResultOf` do
+        openlibs
+        pushUD typeFoo $ Foo 0 "lint"
+        setglobal "foo"
+        dostring "return foo.str" >>= \case
+          OK -> forcePeek $ peekText top
+          _ -> throwErrorAsException
+
+    , "cannot change readonly string" =:
+      "'str' is a read-only property." `shouldBeErrorMessageOf` do
+        openlibs
+        pushUD typeFoo $ Foo 2 "b"
+        setglobal "foo"
+        ErrRun <- dostring "foo.str = 'c'"
+        throwErrorAsException :: Lua ()
+
+    , "Can peek after getting read-only property" =:
+      Foo 144 "gros" `shouldBeResultOf` do
+        openlibs
+        pushUD typeFoo $ Foo 144 "gros"
+        setglobal "foo"
+        OK <- dostring "bar = foo.str"
+        _ <- getglobal "foo"
+        forcePeek $ peekUD typeFoo top
+
+    , "cannot change unknown property" =:
+      "Cannot set unknown property." `shouldBeErrorMessageOf` do
+        openlibs
+        pushUD typeFoo $ Foo 11 "eleven"
+        setglobal "foo"
+        ErrRun <- dostring "foo.does_not_exist = nil"
+        throwErrorAsException :: Lua ()
+
+    , "pairs iterates over properties" =:
+      ["num", "5", "str", "echo", "show", "function"] `shouldBeResultOf` do
+        openlibs
+        pushUD typeFoo $ Foo 5 "echo"
+        setglobal "echo"
+        OK <- dostring $ Char8.unlines
+          [ "local result = {}"
+          , "for k, v in pairs(echo) do"
+          , "  table.insert(result, k)"
+          , "  table.insert("
+          , "    result,"
+          , "    type(v) == 'function' and 'function' or tostring(v)"
+          , "  )"
+          , "end"
+          , "return result"
+          ]
+        forcePeek $ peekList peekText top
+
+    , "absent properties are not included in `pairs`" =:
+      [("num", "number"), ("str", "string"), ("show", "function")]
+      `shouldBeResultOf` do
+        openlibs
+        pushUD typeQux $ Quux 1 "a"
+        setglobal "a"
+        OK <- dostring $ Char8.unlines
+          [ "local result = {}"
+          , "for k, v in pairs(a) do result[#result+1] = {k, type(v)} end"
+          , "return result"
+          ]
+        forcePeek $ peekList (peekPair peekText peekText) top
+    ]
+
+  , testGroup "Bar type"
+    [ "Modifying a table modifies the object" =:
+      Bar [7, 8] `shouldBeResultOf` do
+        openlibs
+        pushUD typeBar $ Bar [7]
+        setglobal "bar"
+        OK <- dostring "table.insert(bar.nums, 8)"
+        _ <- getglobal "bar"
+        forcePeek $ peekUD typeBar top
+
+    , "Use integer index in alias" =:
+      42 `shouldBeResultOf` do
+        openlibs
+        pushUD typeBar $ Bar [42, 5, 23]
+        setglobal "bar"
+        OK <- dostring "return bar.first"
+        forcePeek $ peekIntegral @Int top
+    ]
+
+  , testGroup "lazy list"
+    [ "Access an element of a lazy list stub" =:
+      3 `shouldBeResultOf` do
+        openlibs
+        pushUD typeLazyIntList $ LazyIntList [1,1,2,3,5,8]
+        setglobal "list"
+        _ <- dostring "return (list[4])"
+        forcePeek $ peekIntegral @Int top
+
+    , "Remaining list is not evaluated" =:
+      2 `shouldBeResultOf` do
+        openlibs
+        pushUD typeLazyIntList $ LazyIntList [1,1,2, Prelude.error "CRASH!"]
+        setglobal "list"
+        _ <- dostring "return (list[3])"
+        forcePeek $ peekIntegral @Int top
+
+    , "Out-of-bounds indices return nil" =:
+      (TypeNil, TypeNil) `shouldBeResultOf` do
+        openlibs
+        pushUD typeLazyIntList $ LazyIntList [1,4,9,16]
+        setglobal "list"
+        _ <- dostring "return list[0], list[5]"
+        (,) <$> ltype (nth 1) <*> ltype (nth 2)
+
+    , "Last evaled index is available in __lazylistindex" =:
+      3 `shouldBeResultOf` do
+        openlibs
+        pushUD typeLazyIntList $ LazyIntList [9..17]
+        setglobal "quuz"
+        _ <- dostring "local foo = quuz[3]; return quuz.__lazylistindex"
+        forcePeek $ peekIntegral @Int top
+
+    , "__lazylistindex becomes `false` when all items are evaled" =:
+      False `shouldBeResultOf` do
+        openlibs
+        pushUD typeLazyIntList $ LazyIntList [1..3]
+        setglobal "quuz"
+        _ <- dostring "local foo = quuz[3]; return quuz.__lazylistindex"
+        forcePeek $ peekBool top
+
+    , "Input can be retrieved unchanged" =:
+      LazyIntList [9..17] `shouldBeResultOf` do
+        openlibs
+        pushUD typeLazyIntList $ LazyIntList [9..17]
+        setglobal "ninetofive"
+        _ <- dostring "assert(ninetofive[3] == 11); return ninetofive"
+        forcePeek $ peekUD typeLazyIntList top
+
+    , "List is writable" =:
+      LazyIntList [1, 4, 9, 16] `shouldBeResultOf` do
+        openlibs
+        pushUD typeLazyIntList $ LazyIntList [0,4,9,16]
+        setglobal "list"
+        OK <- dostring "list[1] = 1; return list"
+        forcePeek $ peekUD typeLazyIntList top
+
+    , "List can be extended" =:
+      LazyIntList [1, 4, 9, 16, 25] `shouldBeResultOf` do
+        openlibs
+        pushUD typeLazyIntList $ LazyIntList [1,4,9,16]
+        setglobal "list"
+        OK <- dostring "list[5] = 25; return list"
+        forcePeek $ peekUD typeLazyIntList top
+
+    , "List can be shortened" =:
+      LazyIntList [1, 9, 27, 81] `shouldBeResultOf` do
+        openlibs
+        pushUD typeLazyIntList $ LazyIntList [1, 9, 27, 81, 243]
+        setglobal "list"
+        OK <- dostring "list[5] = nil; return list"
+        forcePeek $ peekUD typeLazyIntList top
+
+    , "Setting element to nil shortenes the list" =:
+      LazyIntList [1, 9, 27] `shouldBeResultOf` do
+        openlibs
+        pushUD typeLazyIntList $ LazyIntList [1, 9, 27, 81, 243]
+        setglobal "list"
+        OK <- dostring "list[4] = nil; return list"
+        forcePeek $ peekUD typeLazyIntList top
+
+    , "Infinite lists are ok" =:
+      233 `shouldBeResultOf` do
+        openlibs
+        let fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+        pushUD typeLazyIntList $ LazyIntList fibs
+        setglobal "fibs"
+        dostring "return fibs[14]" >>= \case
+          OK -> forcePeek $ peekIntegral @Int top
+          _ -> failLua =<< forcePeek (peekString top)
+    ]
+
+  , testGroup "possible properties"
+    [ "tostring Quux" =:
+      "Quux 11 \"eleven\"" `shouldBeResultOf` do
+        openlibs
+        pushUD typeQux $ Quux 11 "eleven"
+        setglobal "quux"
+        _ <- dostring "return tostring(quux)"
+        forcePeek $ peekText top
+    , "show Quux" =:
+      "Quux 11 \"eleven\"" `shouldBeResultOf` do
+        openlibs
+        pushUD typeQux $ Quux 11 "eleven"
+        setglobal "quux"
+        _ <- dostring "return quux:show()"
+        forcePeek $ peekText top
+
+    , "access Quux.num" =:
+      "12" `shouldBeResultOf` do
+        openlibs
+        pushUD typeQux $ Quux 12 "twelve"
+        setglobal "quux"
+        _ <- dostring "return quux.num"
+        forcePeek $ peekText top
+
+    , "access Quux.str" =:
+      "thirteen!" `shouldBeResultOf` do
+        openlibs
+        pushUD typeQux $ Quux 13 "thirteen"
+        setglobal "quux"
+        _ <- dostring "return quux.num"
+        _ <- dostring "quux.str = quux.str .. '!'; return quux.str"
+        forcePeek $ peekText top
+
+    , testGroup "alias"
+      [ "read subelement via alias" =:
+        13.37 `shouldBeResultOf` do
+          openlibs
+          pushUD typeQux $ Quuz (Point 13.37 0) undefined
+          setglobal "quuz"
+          _ <- dostring "return quuz.x"
+          forcePeek $ peekRealFloat @Double top
+      , "set subelement via alias" =:
+        Point 42 1 `shouldBeResultOf` do
+          openlibs
+          pushUD typeQux $ Quuz (Point 1 1) undefined
+          setglobal "quuz"
+          _ <- dostring "quuz.x = 42; return quuz.point"
+          -- msg <- forcePeek $ peekString top
+          -- liftIO $ putStrLn msg
+          forcePeek $ peekPoint top
+      , "read subelement via integer alias" =:
+        13.37 `shouldBeResultOf` do
+          openlibs
+          pushUD typeQux $ Quuz (Point 13.37 0) undefined
+          setglobal "quuz"
+          _ <- dostring "return quuz[1]"
+          forcePeek $ peekRealFloat @Double top
+      , "set subelement via integer alias" =:
+        Point 42 1 `shouldBeResultOf` do
+          openlibs
+          pushUD typeQux $ Quuz (Point 1 1) undefined
+          setglobal "quuz"
+          _ <- dostring "quuz[1] = 42; return quuz.point"
+          forcePeek $ peekPoint top
+      , "non-aliased integer fields are nil" =:
+        TypeNil `shouldBeResultOf` do
+          openlibs
+          pushUD typeQux (Quuz undefined undefined)
+          setglobal "quuz"
+          _ <- dostring "return quuz[3]"
+          ltype top
+      , "absent alias returns `nil`" =:
+        TypeNil `shouldBeResultOf` do
+          openlibs
+          pushUD typeQux (Quux 9 "to five")
+          setglobal "quux"
+          dostring "return quux.x" >>= \case
+            OK -> ltype top
+            _ -> failLua =<< forcePeek (peekString top)
+      , "alias can point to the element itself" =:
+        9 `shouldBeResultOf` do
+          openlibs
+          pushUD typeLazyIntList (LazyIntList [1, 1, 1, 3, 5, 9, 17, 31])
+          setglobal "tribonacci"
+          dostring "return tribonacci.seq[6]" >>= \case
+            OK -> forcePeek $ peekIntegral @Int top
+            _ -> failLua =<< forcePeek (peekString top)
+      ]
+    ]
+  ]
+
+deftype :: LuaError e
+        => Name                              -- ^ type name
+        -> [(Operation, HaskellFunction e)]  -- ^ operations
+        -> [Member e (HaskellFunction e) a]  -- ^ methods
+        -> UDType e (HaskellFunction e) a
+deftype = deftypeGeneric pushHaskellFunction
+
+deftype' :: LuaError e
+         => Name                  -- ^ type name
+         -> [(Operation, HaskellFunction e)]  -- ^ operations
+         -> [Member e (HaskellFunction e) a]  -- ^ methods
+         -> Maybe (ListSpec e a itemtype)  -- ^ list access
+         -> UDTypeWithList e (HaskellFunction e) a itemtype
+deftype' = deftypeGeneric' pushHaskellFunction
+
+-- | Define a (meta) operation on a type.
+operation :: Operation -> HaskellFunction e -> (Operation, HaskellFunction e)
+operation = (,)
+
+-- | Sample product type
+data Foo = Foo Int String
+  deriving (Eq, Show)
+
+-- | Specify behavior of Foo values in Lua.
+typeFoo :: LuaError e => UDType e (HaskellFunction e) Foo
+typeFoo = deftype "Foo"
+  [ operation Tostring show' ]
+  [ property "num" "some number"
+      (pushIntegral, \(Foo n _) -> n)
+      (peekIntegral, \(Foo _ s) n -> Foo n s)
+  , readonly "str" "some string" (pushString, \(Foo _ s) -> s)
+  , methodGeneric "show" show'
+  ]
+  where
+    show' = do
+      foo <- forcePeek $ peekUD typeFoo (nthBottom 1)
+      pushString (show foo)
+      return (NumResults 1)
+
+
+newtype Bar = Bar [Int]
+  deriving (Eq, Show)
+
+typeBar :: LuaError e => UDType e (HaskellFunction e) Bar
+typeBar = deftype "Bar" []
+  [ property "nums" "some numbers"
+    (pushList pushIntegral, \(Bar nums) -> nums)
+    (peekList peekIntegral, \(Bar _) nums -> Bar nums)
+  , alias "first" "first element" ["nums", IntegerIndex 1]
+  ]
+
+newtype LazyIntList = LazyIntList { fromLazyIntList :: [Int] }
+  deriving (Eq, Show)
+
+typeLazyIntList :: LuaError e
+                => UDTypeWithList e (HaskellFunction e) LazyIntList Int
+typeLazyIntList = deftype' "LazyIntList"
+  [ operation Tostring $ do
+      lazyList <- forcePeek $ peekUD typeLazyIntList (nthBottom 1)
+      pushString (show lazyList)
+      return (NumResults 1)
+  ]
+  [ alias "seq" "sequence" [] ]
+  (Just ( (pushIntegral, fromLazyIntList)
+        , (peekIntegral, \_ lst -> LazyIntList lst)
+        ))
+
+--
+-- Sample sum type
+--
+data Qux
+  = Quux Int String
+  | Quuz Point Int
+  deriving (Eq, Show)
+
+data Point = Point Double Double
+  deriving (Eq, Show)
+
+pushPoint :: LuaError e => Pusher e Point
+pushPoint (Point x y) = do
+  newtable
+  pushName "x" *> pushRealFloat x *> rawset (nth 3)
+  pushName "y" *> pushRealFloat y *> rawset (nth 3)
+
+peekPoint :: LuaError e => Peeker e Point
+peekPoint idx = do
+  x <- peekFieldRaw peekRealFloat "x" idx
+  y <- peekFieldRaw peekRealFloat "y" idx
+  return $ x `seq` y `seq` Point x y
+
+showQux :: LuaError e => HaskellFunction e
+showQux = do
+  qux <- forcePeek $ peekQux (nthBottom 1)
+  pushString $ show qux
+  return (NumResults 1)
+
+peekQux :: LuaError e => Peeker e Qux
+peekQux = peekUD typeQux
+
+typeQux :: LuaError e => UDType e (HaskellFunction e) Qux
+typeQux = deftype "Qux"
+  [ operation Tostring showQux ]
+  [ methodGeneric "show" showQux
+  , property "num" "some number"
+      (pushIntegral, \case
+          Quux n _ -> n
+          Quuz _ n -> n)
+      (peekIntegral, \case
+          Quux _ s -> (`Quux` s)
+          Quuz d _ -> Quuz d)
+
+  , possibleProperty "str" "a string in Quux"
+    (pushString, \case
+        Quux _ s -> Actual s
+        Quuz {}  -> Absent)
+    (peekString, \case
+        Quux n _ -> Actual . Quux n
+        Quuz {}  -> const Absent)
+
+  , possibleProperty "point" "a point in Quuz"
+    (pushPoint, \case
+        Quuz p _ -> Actual p
+        Quux {}  -> Absent)
+    (peekPoint, \case
+        Quuz _ n -> Actual . (`Quuz` n)
+        Quux {}  -> const Absent)
+
+  , alias "x" "The x coordinate of a point in Quuz" ["point", "x"]
+  , alias (IntegerIndex 1) "The x coordinate of a point in Quuz" ["point", "x"]
+  ]
